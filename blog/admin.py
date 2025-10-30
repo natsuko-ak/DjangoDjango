121baf67bf7c0a178b0ec43caf0b66c1650e0d01
natsuko-ak <natsuko_akuta3-std@yahoo.co.jp>
2025-10-31 05:51:56 +0900

    Day5:管理画面から記事を登録できるようにした

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post # <- models.pyからPostをインポート
+
+admin.site.register(Post)  # <- Postモデルを管理画面に登録
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100) # 記事タイトル
+    content = models.TextField()             # 記事本文
+    create_at = models.DateTimeField(auto_now_add=True)  # 作成日時
+
+    def __str__(self):
+        return self.title
