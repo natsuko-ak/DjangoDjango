32ff9da85aa6408154bc3d4e9a17e96ab1dc1f89
natsuko-ak <natsuko_akuta3-std@yahoo.co.jp>
2025-11-02 06:51:07 +0900

    Day7 detail page 完成

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'), # 記事一覧
+    path('<int:pk>/', views.detail, name='detail'), # 記事詳細
 ]
